0690e43be19777ab0af04b0548da136089a5f05b
pierrekolins <pierrepneto@gmail.com>
2013-10-02 19:16:02 -0300

    CodigoFonte

---
+++ fonte1/SilverlightSite/SilverlightSite/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightSite
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVideo_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Thank you for your feedback");
+            Page1 pg = new Page1();
+
+            LayoutRoot.Children.Clear();
+
+            LayoutRoot.Children.Add(pg);
+        }
+
+    }
+}
---
+++ fonte1/SilverlightSite/SilverlightSite/PageVideo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace SilverlightSite
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainPage mp = new MainPage();
+            LayoutRoot.Children.Clear();
+            LayoutRoot.Children.Add(mp);
+
+        }
+
+    }
+}
